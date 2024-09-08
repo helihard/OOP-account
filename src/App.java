4eeabb6ffd28e9cb99eb187bb1b49fabcd651d7c
Heli Hård Källoff <vierge.hard@gmail.com>
2024-09-08 19:51:47 +0200

    test program

--- src/App.java
+++ src/App.java
@@ -1,20 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Account savingsAccount = new Account("Namn Namnsson", 1000);
-        Account salaryAccount = new Account("Test Tester", 25000);
+        Account savingsAccount = new Account("Namn Namnsson", 1000.00);
+        Account salaryAccount = new Account(25000.00);
+        Account funAccount = new Account();
 
-        System.out.println(savingsAccount);
+        salaryAccount.setName("Test Tester");
+        funAccount.setName("Test Tester");
+
         System.out.println(salaryAccount);
 
-        salaryAccount.deposit(6000);
-        System.out.println(salaryAccount);
+        funAccount.deposit(6000.00);
+        System.out.println(funAccount);
 
-        savingsAccount.withdraw(500);
+        savingsAccount.withdraw(500.00);
         System.out.println(savingsAccount);
 
         System.out.println(savingsAccount.getName());
-        savingsAccount.setName("Test Tester");
+
+        savingsAccount.setName("Test Testsson");
         System.out.println(savingsAccount.getName());
+
+        System.out.println(salaryAccount);
+        System.out.println(savingsAccount);
+        System.out.println(funAccount);
     }
 }
